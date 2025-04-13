ce3d405f2c0ea078f88fee7bc214271c90aa4329
Yang Lee <lyfantasy92@gmail.com>
2025-04-13 23:34:15 +0800

    [Add] Unit test case
    -Add test case for the edge detection operator
    -Add test case for output result dimension check

--- EdgeDetection.Tests/OperatorTests.cs
+++ EdgeDetection.Tests/OperatorTests.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using EdgeDetection.ImageProcessing;
+﻿using EdgeDetection.ImageProcessing;
 
 namespace EdgeDetection.Tests
 {
@@ -19,33 +18,44 @@ namespace EdgeDetection.Tests
             var op = OperatorFactory.GetOperator("Prewitt");
             Assert.IsInstanceOfType(op, typeof(PrewittOperator));
         }
-
         [TestMethod]
         public void TestEdgeDetection_Sobel()
         {
             byte[,] dummyImage = new byte[3, 3]
             {
-            { 10, 10, 10 },
-            { 10, 10, 10 },
-            { 10, 10, 10 }
+                { 255, 255, 255 },
+                { 0, 0, 0 },
+                { 0, 0, 0 }
             };
             var op = new SobelOperator();
             var result = op.ApplyEdgeDetection(dummyImage);
-            Assert.IsNotNull(result);
+
+            // Ensure output is not all zero
+            bool hasEdge = false;
+            foreach (var val in result)
+            {
+                if (val > 0)
+                {
+                    hasEdge = true;
+                    break;
+                }
+            }
+
+            Assert.IsTrue(hasEdge, "Expected edges to be detected.");
         }
 
         [TestMethod]
         public void TestEdgeDetection_Prewitt()
         {
-            byte[,] image = new byte[3, 3]
+            byte[,] dummyImage = new byte[3, 3]
             {
-            { 255, 255, 255 },
-            { 0, 0, 0 },
-            { 0, 0, 0 }
+                { 255, 255, 255 },
+                { 0, 0, 0 },
+                { 0, 0, 0 }
             };
 
             var op = new PrewittOperator();
-            var result = op.ApplyEdgeDetection(image);
+            var result = op.ApplyEdgeDetection(dummyImage);
 
             // Ensure output is not all zero
             bool hasEdge = false;
@@ -64,13 +74,47 @@ namespace EdgeDetection.Tests
         [TestMethod]
         public void TestSobel_MinimalImage()
         {
-            byte[,] image = new byte[1, 1] 
+            byte[,] image = new byte[1, 1]
             { { 100 } };
 
             var op = new SobelOperator();
             var result = op.ApplyEdgeDetection(image);
             Assert.AreEqual(1, result.GetLength(0));
             Assert.AreEqual(1, result.GetLength(1));
+        }
+
+        [TestMethod]
+        public void TestPrewitt_MinimalImage()
+        {
+            byte[,] image = new byte[1, 1]
+            { { 100 } };
+
+            var op = new PrewittOperator();
+            var result = op.ApplyEdgeDetection(image);
+            Assert.AreEqual(1, result.GetLength(0));
+            Assert.AreEqual(1, result.GetLength(1));
+        }
+
+        [TestMethod]
+        public void TestSobel_OutputDimension()
+        {
+            byte[,] image = new byte[5, 7]; // Width: 5, Height: 7
+
+            var op = new SobelOperator();
+            var result = op.ApplyEdgeDetection(image);
+            Assert.AreEqual(5, result.GetLength(0));
+            Assert.AreEqual(7, result.GetLength(1));
+        }
+
+        [TestMethod]
+        public void TestPrewitt_OutputDimension()
+        {
+            byte[,] image = new byte[5, 7]; // Width: 5, Height: 7
+
+            var op = new PrewittOperator();
+            var result = op.ApplyEdgeDetection(image);
+            Assert.AreEqual(5, result.GetLength(0));
+            Assert.AreEqual(7, result.GetLength(1));
         }
     }
 }
